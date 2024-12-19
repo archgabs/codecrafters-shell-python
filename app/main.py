2d83b800cf419ade5623d1fa6270ee717f91611c
Gabriel <contato.joaogabriel@proton.me>
2024-12-19 11:08:00 -0300

    invalid tests

--- app/main.py
+++ app/main.py
@@ -50,7 +50,7 @@ def main():
                 # Searchs for executable that matches the command name
                 for path in paths:
                     if os.path.isfile(f"{path}/{cmd}"):
-                        out = subprocess.run([f"{path}/{cmd}"], shell=True, capture_output=True)
+                        out = subprocess.run([f"{path}/{cmd}"], shell=True, capture_output=True).stdout
 
                 sys.stdout.write(out) 
 
