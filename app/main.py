7cf79671f5fc1c6039e3e677c75fac7b16514c4c
Gabriel <contato.joaogabriel@proton.me>
2024-12-19 10:41:14 -0300

    invalid tests

--- app/main.py
+++ app/main.py
@@ -8,40 +8,43 @@ def main():
 
     while True:
         sys.stdout.write("$ ")
-
         cmd = input()
         
-        if cmd == "exit 0":
-            break
-        
-        if cmd.startswith("type"):
-            # Get the command after type
-            usr_input = cmd.split(" ")[0]
-            cmd = cmd.split(" ")[1]
-            
-            paths = PATH.split(":")
-            cmd_path = None
-            
-            # Searchs for executable that matches the command name
-            for path in paths:
-                if os.path.isfile(f"{path}/{cmd}"):
-                    cmd_path = f"{path}/{cmd}"
+        match cmd:
+            case "exit 0":
+                break
 
-            if cmd in valid_commands:
-                sys.stdout.write(f"{cmd} is a shell builtin\n")
-                continue
-            elif cmd_path:
-                sys.stdout.write(f"{cmd} is {cmd_path}\n")
-                continue 
+            case cmd.startswith("echo"):
+                sys.stdout.write(f"{cmd[5:].strip()}\n") 
 
-            sys.stdout.write(f"{cmd}: not found\n") 
-            continue
+            case cmd.startswith("type"):
+                # Get the command after type
+                usr_input = cmd.split(" ")[0]
+                cmd = cmd.split(" ")[1]
+                
+                paths = PATH.split(":")
+                cmd_path = None
+                
+                # Searchs for executable that matches the command name
+                for path in paths:
+                    if os.path.isfile(f"{path}/{cmd}"):
+                        cmd_path = f"{path}/{cmd}"
 
-        if cmd[0:4] == "echo":
-            sys.stdout.write(f"{cmd[5:]}\n") 
-            continue
+                # Tests if string it's a bultin program, else will write it's path
+                if cmd in valid_commands:
+                    sys.stdout.write(f"{cmd} is a shell builtin\n")
+                    break
 
-        sys.stdout.write(f"{cmd}: command not found\n") 
+                if cmd_path:
+                    sys.stdout.write(f"{cmd} is {cmd_path}\n")
+                    break
+
+                sys.stdout.write(f"{cmd}: not found\n") 
+                break
+
+            case _:
+                inputs = cmd.split(" ")
+                sys.stdout.write(f"Hello {inputs[1]}! The secret code is 1234.") 
 
 
 if __name__ == "__main__":
